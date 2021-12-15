5d69cf14253df36d9052142f7ba0b49a4b7ddd7d
sarahekd <92767247+sarahekd@users.noreply.github.com>
2021-12-15 13:45:27 -0800

    Update views.py
    Added error messages to home and edit templates for failed validation checks

--- todo_list/views.py
+++ todo_list/views.py
@@ -14,43 +14,55 @@ def home(request):
             messages.success(request, ('Item Has Been Added To List!'))
             return render(request, 'home.html', {'all_items': all_items})
 
+        else:
+            all_items = List.objects.all
+            err = form.errors
+            messages.error(request, ({'err':err}))
+            return render(request, 'home.html', {'all_items': all_items})
+
     else:
         all_items = List.objects.all
         return render(request, 'home.html', {'all_items': all_items})
 
 def about(request):
-	context = {'first_name': 'Sarah', 'last_name': 'Key-DeLyria'}
-	return render(request, 'about.html', context)
+    context = {'first_name': 'Sarah', 'last_name': 'Key-DeLyria'}
+    return render(request, 'about.html', context)
 
 def delete(request, list_id):
-	item = List.objects.get(pk=list_id)
-	item.delete()
-	messages.success(request, ('Item Has Been Deleted!'))
-	return redirect('home')
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item Has Been Deleted!'))
+    return redirect('home')
 
 def cross_off(request, list_id):
-	item = List.objects.get(pk=list_id)
-	item.completed = True
-	item.save()
-	return redirect('home')
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
 
 def uncross(request, list_id):
-	item = List.objects.get(pk=list_id)
-	item.completed = False
-	item.save()
-	return redirect('home')
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
 
 def edit(request, list_id):
-	if request.method == 'POST':
-		item = List.objects.get(pk=list_id)
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
 
-		form = ListForm(request.POST or None, instance=item)
+        form = ListForm(request.POST or None, instance=item)
 
-		if form.is_valid():
-			form.save()
-			messages.success(request, ('Item Has Been Edited!'))
-			return redirect('home')
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Item Has Been Edited!'))
+            return redirect('home')
 
-	else:
-		item = List.objects.get(pk=list_id)
-		return render(request, 'edit.html', {'item': item})
+        else:
+            all_items = List.objects.all
+            err = form.errors
+            messages.error(request, ({'err':err}))
+            return render(request, 'home.html', {'all_items': all_items})
+
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item})
