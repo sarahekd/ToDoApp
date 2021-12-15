9f88a54c21fe66066360dfed86f50197248481c7
sarahekd <92767247+sarahekd@users.noreply.github.com>
2021-12-15 13:44:04 -0800

    Update models.py
    Improved functionality of the DateTimeField
    Added a priority feature

--- todo_list/models.py
+++ todo_list/models.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 
 class List(models.Model):
     item = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
-    time_est= models.DecimalField(max_digits=5, decimal_places=2, default=0)
-    notes= models.CharField(max_length=10000, default="Notes")
-    due_date = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False, blank=True)
+    time_est= models.DecimalField(max_digits=5, decimal_places=2, default=0.0, blank=True)
+    notes= models.CharField(max_length=10000, default="Notes", blank=True)
+    due_date = models.DateTimeField(null=True, default=timezone.now, blank=True)
+    priority = models.CharField(max_length=200, default="Priority", blank=True) #eventually I'll switch this to a drop down with choices
         
     def __str__(self):
         return self.item + ' | ' + str(self.completed)
